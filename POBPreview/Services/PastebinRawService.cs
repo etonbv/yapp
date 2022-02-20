97ca83594b99914e6715848dcdebf678b7132866
edsonbarojas <eton.b@hotmail.com>
2022-02-19 19:19:20 -0600

    More cors stuff..

--- POBPreview/Services/PastebinRawService.cs
+++ POBPreview/Services/PastebinRawService.cs
@@ -12,11 +12,7 @@ namespace POBPreview
         {
             HttpClient web = new HttpClient();
 			string pasteKey = PastebinRegex.Match(url).Groups["key"].Value;
-#if DEBUG
-			return await web.GetStringAsync($"https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/{pasteKey}");
-#else
 			return await web.GetStringAsync($"https://pastebin.com/raw/{pasteKey}");
-#endif
 		}
 	}
 }
